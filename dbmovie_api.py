0836f7ba29c8a79374c734993e2722e237de775d
kentsx <admin@kmore.ml>
2020-02-17 23:12:23 +0800

    theadpool不会报错 但关于超过50个作品的问题，暂未解决。

--- dbmovie_api.py
+++ dbmovie_api.py
@@ -2,21 +2,24 @@
 import requests, json
 import os
 import threading
+from multiprocessing.pool import ThreadPool
+
+def write_essential(name,id,count='50'):
+	url = 'https://douban.uieee.com/v2/movie/celebrity/'+id+'/works?start=1&count='+count
+	response = requests.get(url, headers = headers)
+	content = json.loads(response.text)
+
+	data = json.dumps(content,ensure_ascii=False,indent =2)
+	with open(name,'w',encoding='utf-8') as f:
+		f.write(data)
+
 def db_write(name,id,count='50'):
 	try:
 		file_name = name + id +'.json'
 
 		if file_name not in file_list:
-			url = 'https://douban.uieee.com/v2/movie/celebrity/'+id+'/works?start=1&count='+count
-			response = requests.get(url, headers = headers)
-
-			content = json.loads(response.text)
-
-			data = json.dumps(content,ensure_ascii=False,indent =2)
-			with open(file_name,'w',encoding='utf-8') as f:
-				f.write(data)
-		else:
-			pass
+			write_essential(name=file_name, id=id)
+			
 	except TypeError as e:
 		print (name)
 
@@ -25,12 +28,13 @@ def db_extract(file):
 		data = json.load(f)
 	works_num = data['total']
 	## 如果作品超过51部，需要再获取作品数据
+	if works_num > 51:
+		print('--------------%s--More than 51----------' % celeb_name)
+		# write_essential(file ,celeb_dbid,count=str(works_num))
+
 	celeb_name = data['celebrity']['name']
 	celeb_name_en = data['celebrity']['name_en']
 	celeb_dbid = data['celebrity']['id']
-	if works_num > 51:
-		print('--------------%s--More than 51----------' % celeb_name)
-		db_write(name,id,count=str(works_num))
 	works = data['works']
 	for work in works:
 		casts = work['subject']['casts']
@@ -52,10 +56,14 @@ if __name__ == '__main__':
 	file_list = os.listdir(file_path)
 	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 	## 限制线程数为5
-	sem = threading.Semaphore(5)
-	with sem:
-		for file in file_list:
+	# sem = threading.Semaphore(5)
+	# with sem:
+	pool = ThreadPool(processes = 10)
+	pool.map(db_extract,(file for file in file_list))
+	pool.close()
+		# for file in file_list:
 			# sem.acquire()
-			t1 = threading.Thread(target =db_extract, args = (file,))
-			t1.start()
+			# t1 = threading.Thread(target =db_extract, args = (file,))
+			# t1.start()
 			# sem.release()
+			# db_extract(file)
